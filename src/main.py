29ec1d115b96a85cc216a4e38b04d597511019a7
Omer Priel <omer2079@gmail.com>
2021-12-07 13:10:59 +0200

    Init commit

---
+++ src/main.py
@@ -0,0 +1,17 @@
+# System
+import os
+
+# Web Scraping
+import requests
+from bs4 import BeautifulSoup
+
+# Data
+import numpy as np
+import pandas as pd
+
+# View
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# ML
+import sklearn
