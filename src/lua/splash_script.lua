b1706f94213f6781d765af09fd462d4a2a0d17ff
Omer Priel <omer2079@gmail.com>
2022-02-02 12:53:05 +0200

    try splash with lua

--- src/lua/splash_script.lua
+++ src/lua/splash_script.lua
@@ -1,15 +1,29 @@
 function main(splash)
+  function wait_for(splash, condition)
+    while not condition() do
+      splash:wait(0.05)
+    end
+  end
+
+  local btnSelector = 'section[aria-label="Amenities and More"] button[aria-controls]'
+  local atterSelector = 'section[aria-label="Amenities and More"] span'
+
+  function atters_extra_exist()
+    return splash:evaljs("document.querySelector(\'".. btnSelector .. "\') != null")
+  end
+
+  function click_atters()
+    local command = "document.querySelector(\'" .. btnSelector .. "\').click();"
+    splash:runjs(command)
+    return splash:evaljs("document.querySelector(\'" .. btnSelector .. "\').textContent == \'Show Less\'")
+  end
+
   local url = splash.args.url
   assert(splash:go(url))
   assert(splash:wait(0.5))
-  local sOpen = "setTimeout(() => {"
-  local s1 = "document.querySelector(\'yelp-react-root div section[aria-label=\"Amenities and More\"]"
-  local s2 = " button[type=\"submit\"]\').click();"
-  local sEnd = "}, 3000);"
-  local command = sOpen .. s1 .. s2 .. "document.title='';" .. sEnd
-  splash:runjs(command)
-  splash:wait(10)
-  local elements = splash:select('body')
+  wait_for(splash, atters_extra_exist)
+  wait_for(splash, click_atters)
+  local elements = splash:select_all('yelp-react-root div section[aria-label=\"Amenities and More\"] span')
   local html = splash:html()
-  return {command=command, elements=elements:info()}
+  return {command=command, elements=#elements}
 end
