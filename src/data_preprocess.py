5dc859521a770a37adf7ab2fa6ce3cc42a18cb69
Omer Priel <omer2079@gmail.com>
2022-02-08 10:58:53 +0200

    first clean of the data

--- src/data_preprocess.py
+++ src/data_preprocess.py
@@ -11,9 +11,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def reorganize_index():
-    df = pd.read_csv('../data/businesses.csv')
-    df[id] = df.index
-    df.to_csv('../data/businessestmp.csv')
+#
+#def reorganize_index():
+    #df = pd.read_csv('../data/businesses.csv')
+    #df[id] = df.index
+    #df.to_csv('../data/businessestmp.csv')
 
 # reorganize_index()
+
+# Utilities Files
+def read_csv(name: str, index_label='id') -> pd.DataFrame:
+    return pd.read_csv('../data/' + name + '.csv', index_col=index_label)
+
+
+def save_csv(df: pd.DataFrame, name: str, index_label='id'):
+    df.to_csv('../data/' + name + '.csv', index_label=index_label)
+
+# Load the Data
+df = read_csv('businesses')
+
+# Config view Settings
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_columns',15)
+
+# Reorder
+df.drop(columns=['MenuCount', 'MenuStartsCount', 'MenuReviewsCount', 'MenuPhotosCount'], inplace=True)
+
+df['Attributes Has'] = df['Attributes Has'].astype(object)
+
+for i in df[df.isna()['Attributes Has']].index:
+    df.at[i, 'Attributes Has'] = np.arange(0)
+    df.at[i, 'AttributesCount'] = 0
+
+
+# Save
+save_csv(df, 'businessestmp')
