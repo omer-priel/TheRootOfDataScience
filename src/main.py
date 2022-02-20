766df9dfef67f02dcaa0cc774fa8898645e666d5
Omer Priel <omer2079@gmail.com>
2022-02-20 23:41:07 +0200

    start training

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-# Before Production
+#%%
 
 # System
 import os
@@ -14,7 +14,34 @@ import pandas as pd
 
 # View
 import matplotlib.pyplot as plt
+import matplotlib.patches
 import seaborn as sns
+from IPython.display import Image
 
 # ML
 import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn import metrics
+
+# ML Models
+from sklearn.dummy import DummyClassifier
+
+from sklearn.ensemble import VotingClassifier
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import Lasso
+from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.svm import OneClassSVM
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+
+# Intel CPU
+from sklearnex import patch_sklearn
